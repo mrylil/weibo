da7cd5a93571715d1e17164d128181b38de7966b
vagrant <vagrant@homestead>
2024-01-10 04:28:22 +0000

    切割头部和尾部子视图

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,3 +1,5 @@
+
+
 <footer class="footer">
   <img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
   <a href="https://learnku.com/laravel/courses" target=_blank class="text-decoration-none">
