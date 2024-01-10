a3c58dce8afac986b7e96ce94303b88aa3c16c81
vagrant <vagrant@homestead>
2024-01-10 14:25:13 +0000

    中文的错误提示信息

---
+++ lang/zh_CN/validation.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    'attributes' => [
+        'name' => '名称',
+        'email' => '邮箱',
+        'password' => '密码',
+    ],
+    'custom' => [
+        'name' => [
+            'required' => '请填写名称！',
+            
+        ], 'email' => [
+            'required' => '请填写邮箱！',
+            
+        ],
+        'password' => [
+            'required' => '请填写密码！',
+            
+        ],
+    ],
+];
