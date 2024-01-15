421ba6de3aeac6411ee86eeaf8a6252412de7a43
vagrant <vagrant@homestead>
2024-01-15 13:15:13 +0000

    查看用户列表

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +22,13 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+      
+            Model::unguard();
+    
+            $this->call(UsersTableSeeder::class);
+    
+            Model::reguard();
+        
     }
 }
